450adfd1296e6c22e94a63d788a830d26679b5b6
bregy1 <31240559+bregy1@users.noreply.github.com>
2025-08-20 22:09:03 +0200

    add first day exercises

---
+++ projects/day1/loesung/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student std1 = new Student("Jonas", "Summermatter", 16);
+        Student std2 = new Student("Peter", "Bregy", 19);
+        Student std3 = new Student("Ullrich", "Vondenblatten", 22);
+
+        std1.info();
+        std2.info();
+        std3.info();
+
+    }
+
+}
